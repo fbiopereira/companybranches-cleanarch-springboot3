3cc471a1e754738e9e10d33505aa18082112af19
Fábio Pereira <fabio.pereira@outlook.com>
2023-04-04 20:25:19 -0300

    project creation

---
+++ companybranches-springboot3/src/main/java/com/fbiopereira/companybranchesspringboot3/CompanybranchesSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.fbiopereira.companybranchesspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanybranchesSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanybranchesSpringboot3Application.class, args);
+    }
+
+}
---
+++ companybranches-springboot3/src/test/java/com/fbiopereira/companybranchesspringboot3/CompanybranchesSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbiopereira.companybranchesspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanybranchesSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
