87d691dc923182fb971b8b6bcada2f1e731dfa40
Fábio Pereira <fabio.pereira@outlook.com>
2023-05-16 20:30:59 -0300

    project packages creation

---
+++ src/main/java/com/fbiopereira/companybranchescleanarchspringboot3/CompanybranchesCleanarchSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.fbiopereira.companybranchescleanarchspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanybranchesCleanarchSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanybranchesCleanarchSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fbiopereira/companybranchescleanarchspringboot3/CompanybranchesCleanarchSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbiopereira.companybranchescleanarchspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanybranchesCleanarchSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
