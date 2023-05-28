5c536767863d1aa53961ecdce138026ff8deeac1
Fábio Pereira <fabio.pereira@outlook.com>
2023-05-28 20:49:23 -0300

    Feign integration test - success

--- src/test/java/com/fbiopereira/companybranchescleanarchspringboot3/integration/dataprovider/rest/client/EnderecoApiClientTest.java
+++ src/test/java/com/fbiopereira/companybranchescleanarchspringboot3/integration/dataprovider/rest/client/EnderecoApiClientTest.java
@@ -5,8 +5,7 @@ import com.fbiopereira.companybranchescleanarchspringboot3.configuration.Wiremoc
 import com.fbiopereira.companybranchescleanarchspringboot3.dataprovider.rest.client.EnderecoApiClient;
 import com.fbiopereira.companybranchescleanarchspringboot3.dataprovider.rest.response.EnderecoApiResponse;
 import com.github.tomakehurst.wiremock.WireMockServer;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.IOException;
 import java.net.URL;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
@@ -31,23 +31,38 @@ public class EnderecoApiClientTest {
     @Value("${fbiopereira.enderecoApiClient.apiUrl}")
     String enderecoApiUrl;
 
+    URL enderecoApiUrlObject;
+
     @Autowired
    private EnderecoApiClient enderecoApiClient;
 
     @Before
     public void setUp() throws Exception {
-        URL enderecoApiUrlObject = new URL(enderecoApiUrl);
-        wireMockServer = WiremockConfiguration.build(enderecoApiUrlObject);
 
+        enderecoApiUrlObject = new URL(enderecoApiUrl);
+        wireMockServer = WiremockConfiguration.build(enderecoApiUrlObject);
+        wireMockServer.start();
+
+    }
+
+    @After
+    public void finish(){
+        wireMockServer.shutdown();
     }
 
 
     @Test
-    @DisplayName("Integration Test for EnderecoApiClient - get Cep with value 01234000")
+    @DisplayName("Integration Test for EnderecoApiClient get Cep with value 01234000")
     public void testEnderecoApiClientGetCep() throws IOException {
-        WiremockConfiguration.wireMockClientResponseMock(wireMockServer, "enderecoApi/cep/01234000", HttpStatus.OK.value(), "payload/enderecoApiClient-getCep-01234000.json");
-        wireMockServer.start();
-        EnderecoApiResponse enderecoApiResponse = enderecoApiClient.find("01234000");
-        assertNotNull(enderecoApiResponse);
+       String endpoint = enderecoApiUrlObject.getPath() + "/cep/01234000";
+       WiremockConfiguration.wireMockClientResponseMock(wireMockServer, endpoint, HttpStatus.OK.value(), "payload/enderecoApiClient-getCep-01234000.json");
+       EnderecoApiResponse enderecoApiResponse = enderecoApiClient.find("01234000");
+
+       assertNotNull(enderecoApiResponse);
+       assertEquals("01234-000", enderecoApiResponse.getCep());
+       assertEquals("Avenida Pacaembu", enderecoApiResponse.getLogradouro());
+       assertEquals("Pacaembu", enderecoApiResponse.getBairro());
+       assertEquals("São Paulo", enderecoApiResponse.getCidade());
+       assertEquals("SP", enderecoApiResponse.getUf());
     }
 }
